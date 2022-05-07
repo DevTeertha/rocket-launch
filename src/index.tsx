2374c6de929084f010e678e2150a99ae3a778eb8
DevTeertha <devteertha28@gmail.com>
2022-05-08 04:10:46 +0600

    project setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import SSRProvider from 'react-bootstrap/SSRProvider';
+import ReactDOMClient from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import './index.css';
+import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+const root = ReactDOMClient.createRoot(
+    document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <SSRProvider>
+                <App />
+            </SSRProvider>
+        </Provider>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/reducers/launchesReducer.ts
@@ -0,0 +1,35 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const getLaunches: any = createAsyncThunk(
+    'launches/getLaunches',
+    async () =>
+        fetch('https://api.spacexdata.com/v3/launches').then((res) =>
+            res.json(),
+        ),
+);
+
+const launchesSlice = createSlice({
+    name: 'Launch Slice',
+    initialState: {
+        status: null,
+        loading: false,
+        launches: [],
+    },
+    reducers: {},
+    extraReducers: {
+        [getLaunches.pending]: (state: any) => {
+            state.status = 'loading';
+            state.loading = true;
+        },
+        [getLaunches.fulfilled]: (state: any, action) => {
+            state.status = 'success';
+            state.loading = false;
+            state.launches = action.payload;
+        },
+        [getLaunches.rejected]: (state: any) => {
+            state.status = 'error';
+            state.loading = false;
+        },
+    },
+});
+export default launchesSlice.reducer;
